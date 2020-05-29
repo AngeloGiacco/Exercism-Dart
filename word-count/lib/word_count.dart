9c5c40ceb41a302e8bbce0f15c25c1d87d9126d1
Angelo Giacco <giacco.angelo.s@gmail.com>
2020-05-29 11:28:27 +0100

    Update word_count.dart

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -4,11 +4,10 @@ class WordCount {
     final wordCount = <String, int>{};
     RegExp exp = RegExp(r"\w+(\'\w+)?");
     Iterable<RegExpMatch> matches = exp.allMatches(text);
-    matches.fold(
-        0,
-        (dynamic a, b) => wordCount
+    return matches.fold<Map<String, int>>(
+        wordCount,
+        (a, b) => wordCount
           ..update(b.group(0).toLowerCase(), (value) => value + 1,
               ifAbsent: () => 1));
-    return wordCount;
   }
 }
